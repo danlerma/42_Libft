e9a9fd007b0076fc1c794e10af780ba544f0af85
Dan <daniellerma05218@gmail.com>
2021-08-24 17:50:45 +0200

    Ultimos cambios
    Cambios antes de subirlo cabiando el split

--- Libft/ft_lstadd_back_bonus.c
+++ Libft/ft_lstadd_back_bonus.c
@@ -6,17 +6,21 @@
 /*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 13:40:51 by dlerma-c          #+#    #+#             */
-/*   Updated: 2021/08/24 15:54:31 by dlerma-c         ###   ########.fr       */
+/*   Updated: 2021/08/24 16:59:00 by dlerma-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    if (*lst)
-    {
-        ft_lstlast(*lst);
-        (void)new;
-    }
+	t_list	*aux;
+
+	if (*lst == NULL)
+		*lst = new;
+	else if (*lst)
+	{
+		aux = ft_lstlast(*lst);
+		aux->next = new;
+	}
 }
--- Main/comStrnstr.c
+++ Main/comStrnstr.c
@@ -4,9 +4,9 @@
 void	comStrnstr(void)
 {
 	size_t	max = strlen("hola mundo");
-	//char	*i1 = strnstr((void *)0), "fake", max);
-	char	*i2 = ft_strnstr((void *)0, "fake", 3);
+	char	*i1 = strnstr("hola mundo", "a", max);
+	char	*i2 = ft_strnstr("hola mundo", "a", max);
 
-	//printf("\nOriginal (len = %zu): %s\n", max, i1);
+	printf("\nOriginal (len = %zu): %s\n", max, i1);
 	printf("\nFuncion  (len = %zu): %s\n", max, i2);
 }
